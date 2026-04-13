f2f8b22559acf1c1b5a15f340efebf8e1488c31a
noureddine makboul <152820039+makbouldev@users.noreply.github.com>
2026-04-13 16:46:47 +0100

    Fix prerender plugin import for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { prerender } from 'vite-plugin-prerender'
+import prerender from 'vite-plugin-prerender'
 
 // https://vite.dev/config/
 export default defineConfig({
